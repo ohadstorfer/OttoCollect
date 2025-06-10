c648a05c148b1e0b332b7c1ff60a0990fb2e3923
ohadstorfer <ohad.storfer@gmail.com>
2025-06-10 22:09:00 +0300

    compare images

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,4 +1,3 @@
-
 import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
@@ -10,14 +9,14 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default: "bg-primary text-primary-foreground hover:bg-gold-700",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+          "border border-input bg-background hover:bg-accent hover:bg-gold-400",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
+        ghost: "hover:bg-accent hover:bg-gold-400",
         link: "text-primary underline-offset-4 hover:underline",
         primary: "bg-ottoman-600 text-white hover:bg-ottoman-700",
       },
