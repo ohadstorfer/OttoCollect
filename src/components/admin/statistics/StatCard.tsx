38e4792306058149cfbe1e16cd3e3cbc7880211b
ohadstorfer <ohad.storfer@gmail.com>
2025-08-02 15:35:16 +0800

    STATISTICS

--- src/components/admin/statistics/StatCard.tsx
+++ src/components/admin/statistics/StatCard.tsx
@@ -30,7 +30,7 @@ export const StatCard: React.FC<StatCardProps> = ({
   return (
     <Card>
       <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-        <CardTitle className="text-sm font-medium">{title}</CardTitle>
+        <CardTitle className="text-sm font-medium"> <span>{title}</span></CardTitle>
         <Icon className="h-4 w-4 text-muted-foreground" />
       </CardHeader>
       <CardContent>
